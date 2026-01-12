9440c53a50df1d4002c2073cd5af83736aa3705f
Disha Giri <dishagiri09170@gmail.com>
2026-01-12 12:28:59 +0530

    media quaries

--- todo/script.js
+++ todo/script.js
@@ -3,24 +3,20 @@ const input = document.getElementById("taskInput");
 const taskList = document.getElementById("task-list");
 
 addBtn.addEventListener("click", () => {
-  // stop if textarea empty
   if (input.value.trim() === "") return;
 
   const taskRow = document.createElement("div");
   taskRow.className = "task-item";
 
   taskRow.innerHTML = `
-
     <p class="task-text">${input.value}</p>
     <span class="delete">✕</span>
   `;
 
   taskList.appendChild(taskRow);
 
-  // CLEAR TEXTAREA
   input.value = "";
 
-  // delete task
   taskRow.querySelector(".delete").addEventListener("click", () => {
     taskRow.remove();
   });
